73b72f51c083a82986fbb9bf1228c3c4322dc773
xtremonius <alejandro.aquino1988@gmail.com>
2022-01-17 15:50:30 -0300

    Reading file txt

--- cypress/integration/search.js
+++ cypress/integration/search.js
@@ -1,18 +1,31 @@
-
-describe('Search elements', ()=>{
-    beforeEach(()=>{
-        cy.visit('/')
-    })
-    it('search for elements with multiple results', ()=>{
-        cy.search('dress')
-        cy.fixture('searchResult').then((searchResult)=>{
-            cy.get(searchResult.title).should('contain','dress');
-        })
-    })
-    it('search for elements with no results', ()=>{
-        cy.search('qwerty')
-        cy.fixture('searchResult').then((searchResult)=>{
-            cy.get(searchResult.alert).should('contain','No results were found for your search');
-        })
-    })
-})
+describe("Search elements", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+  it("search for elements with multiple results", () => {
+    cy.search("dress");
+    cy.fixture("searchResult").then((searchResult) => {
+      cy.get(searchResult.title).should("contain", "dress");
+    });
+  });
+  it("search for elements with no results", () => {
+    cy.search("qwerty");
+    cy.fixture("searchResult").then((searchResult) => {
+      cy.get(searchResult.alert).should(
+        "contain",
+        "No results were found for your search"
+      );
+    });
+  });
+  it("search for elements with special code", () => {
+    cy.readFile("cypress/support/text/search.txt").then((text) => {
+      cy.search(text);
+    });
+    cy.fixture("searchResult").then((searchResult) => {
+      cy.get(searchResult.alert).should(
+        "contain",
+        "No results were found for your search"
+      );
+    });
+  });
+});
